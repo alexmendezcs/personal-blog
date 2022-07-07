beacd88536d72b1c3fb68fbf840f88ba8441b454
alexmendezcs <alexmendezcs@gmail.com>
2022-07-07 09:48:39 -0500

    Added a base template, css and bootstrap

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'AlexCS', 
+        'title': 'Blog Post 1', 
+        'content': 'First Post Content', 
+        'date_posted': 'July 1, 2022'
+    }, 
+    {
+        'author': 'Claxton Doe', 
+        'title': 'Blog Post 2', 
+        'content': 'Second Post Content', 
+        'date_posted': 'July 4, 2022'
+    }
+]
 
 def home(request): 
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request): 
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
+
