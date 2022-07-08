2ba9b92d35b4aed6d17de829cf62f62593fb63e1
alexmendezcs <alexmendezcs@gmail.com>
2022-07-08 07:53:29 -0500

    Created database tables using Django models.

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'AlexCS', 
-        'title': 'Blog Post 1', 
-        'content': 'First Post Content', 
-        'date_posted': 'July 1, 2022'
-    }, 
-    {
-        'author': 'Claxton Doe', 
-        'title': 'Blog Post 2', 
-        'content': 'Second Post Content', 
-        'date_posted': 'July 4, 2022'
-    }
-]
+from .models import Post
 
 def home(request): 
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
